22d77fa387f950be7c6692b0c236c16dffd245fa
Yohannes Mengistie <yohannesmengistie634@gmail.com>
2024-10-08 23:58:05 +0300

    setuping the project

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import "@fontsource/outfit"
+import "@fontsource/roboto"
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
